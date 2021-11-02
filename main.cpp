b85f415db2107217bd8256ea012a7d5e4680858c
Emily <56365208+e457@users.noreply.github.com>
2021-11-02 15:03:38 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//C++ TIC-TAC-TOE 
+
+//Header files included 
+#include <iostream> 
+using namespace std; 
+//There isn't much std::cout in this project, so namespace was used
+
+
+//variable and their data structures contained within a class
